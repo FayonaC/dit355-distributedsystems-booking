b46697ceb9ae24ccc033b8f9a5ceaa2ff0324065
fayona <fayona@student.chalmers.se>
2020-11-30 10:42:54 +0100

    Add pub-sub functionality
    Jenni and I added pub-sub functionality so that the booking component can now communicate via MQTT.
    
    Resolves: #3

--- src/main/java/Coordinator.java
+++ src/main/java/Coordinator.java
@@ -1,7 +1,17 @@
+import org.eclipse.paho.client.mqttv3.MqttException;
+
 public class Coordinator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MqttException {
+
+        Subscriber s = new Subscriber();
+        s.subscribeToMessages();
+
         DataAccessLayer dal = new DataAccessLayer();
-        BookingRegistry bookingsJSON = dal.loadBookingRegistry();
-        System.out.print(bookingsJSON);
+        BookingRegistry bookingsJson = dal.loadBookingRegistry();
+        System.out.print(bookingsJson);
+
+        Publisher p = new Publisher();
+        p.sendMessage(bookingsJson);
+        p.close();
     }
 }
--- src/main/java/Publisher.java
+++ src/main/java/Publisher.java
@@ -1,2 +1,33 @@
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.MqttPersistenceException;
+
 public class Publisher {
+
+    private final static String TOPIC = "BookingResponse";
+
+    private final static String BROKER = "tcp://localhost:1883";
+
+    private final static String USER_ID = "booking-publisher";
+
+    private final IMqttClient middleware;
+
+    public Publisher() throws MqttException {
+        middleware = new MqttClient(BROKER, USER_ID);
+        middleware.connect();
+    }
+
+    void close() throws MqttException {
+        middleware.disconnect();
+        middleware.close();
+    }
+
+    void sendMessage(BookingRegistry messageTest) throws MqttPersistenceException, MqttException {
+        MqttMessage message = new MqttMessage();
+        String msg = messageTest.toString();
+        message.setPayload(msg.getBytes()); /*move messageTest into payload*/
+        middleware.publish(TOPIC, message);
+    }
 }
