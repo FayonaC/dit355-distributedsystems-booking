f92c9530a933494d83e5380951ff18263fffd0a8
hashmati <hashmati@student.chalmers.se>
2020-12-15 15:05:11 +0100

    Update validation in setters
    Updated the code for validation in the Booking setters as per the review comments made by Jenni.
    
    Resolves #6

--- src/main/java/Booking.java
+++ src/main/java/Booking.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Booking {
 
     private long userid;
@@ -6,34 +9,25 @@ public class Booking {
     private long issuance;
     private String time;
 
-    /**
-     * TODO: Change so constructor uses setters.
-     * Before: this.userid = userid;
-     * After: setUserid(userid);
-     */
     public Booking(long userid, long requestid, long dentistid, long issuance, String time) {
-        this.userid = userid;
-        this.requestid = requestid;
-        this.dentistid = dentistid;
-        this.issuance = issuance;
-        this.time = time;
+        setUserid(userid);
+        setRequestid(requestid);
+        setDentistid(dentistid);
+        setIssuance(issuance);
+        setTime(time);
     }
 
     public long getUserid() {
         return userid;
     }
 
-    /**
-     * Doesn't allow user id's to be shorter than 5 digits and we have some that are just 1 digit.
-     * It would be enough to check userid < 100000 since that would make it 5 digits max. (same as in dentist)
-     */
     public void setUserid(long userid) {
         String userValidation = String.valueOf(userid); // Coverts the long userid to a String to be used for validation
-        if (userValidation.matches("[0-9]{5}")) {
+        if (userValidation.matches("[0-9]{1,5}")) { // This regex might have to change if we change the format for user id's
             this.userid = userid;
         }
         else {
-            throw new IllegalArgumentException("User id has to  be five digits long");
+            throw new IllegalArgumentException("User id has to  be between one and five digits long");
         }
     }
 
@@ -41,16 +35,13 @@ public class Booking {
         return requestid;
     }
 
-    /**
-     * Not sure if a user can make 99 requests maximum or if this should be higher than 2 digits.
-     */
     public void setRequestid(long requestid) {
         String requestValidation = String.valueOf(requestid); // Coverts the long requestid to a String to be used for validation
-        if (requestValidation.matches("[0-9]{1,2}")) {
+        if (requestValidation.matches("[0-9]{1,5}")) { // This allows there to be up to 99999 requests
             this.requestid = requestid;
         }
         else {
-            throw new IllegalArgumentException("Request id has to be between one and two digits long");
+            throw new IllegalArgumentException("Request id has to be between one and five digits long");
         }
     }
 
@@ -86,16 +77,14 @@ public class Booking {
         return time;
     }
 
-    /**
-     * Accepts "test" as a correct format.
-     * Perhaps try using LocalDateTime.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")) with no if statement
-     * but a try-catch around it. Try parsing & setting the time and if it fails then throw the illegalArgument...
-     */
     public void setTime(String time) {
-        if(time.matches(".*[0-9][-][:]{16}")) {
-            throw new IllegalArgumentException("Time has to be in the format YYYY-MM-DD 00:00 (16 characters long including spaces, dashes, and colons)");
+        try {
+            LocalDateTime.parse(time, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"));
+            this.time = time;
         }
-        this.time = time;
+        catch (IllegalArgumentException e) {
+            System.out.println("Time has to be in the format YYYY-MM-DD 00:00 (16 characters long including spaces, dashes, and colons)");
+        }
     }
 
     @Override
